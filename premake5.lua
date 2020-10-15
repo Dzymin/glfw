de931f9758e7bbe98e1500712e135fec65d24210
Dzymin <szymonsobczak5@gmail.com>
2020-10-15 21:12:32 +0200

    Added premake script

---
+++ premake5.lua
@@ -0,0 +1,42 @@
+project "GLFW"
+    kind "StaticLib"
+    language "C"
+
+    targetdir ("Bin/" .. OutputDir .. "/%{prj.name}")
+    objdir ("Obj/" .. OutputDir .. "/%{prj.name}")
+
+    files {
+        "include/GLFW/glfw3.h",
+        "include/GLFW/glfw3native.h",
+        "src/context.c",
+        "src/init.c",
+        "src/input.c",
+        "src/monitor.c",
+        "src/vulkan.c",
+        "src/window.c",
+        "src/osmesa_context.c",
+        "src/wgl_context.c",
+        "src/egl_context.c"
+
+    }
+
+    filter "system:windows"
+        files {
+			"src/win32_init.c",
+			"src/win32_joystick.c",
+			"src/win32_monitor.c",
+			"src/win32_time.c",
+			"src/win32_thread.c",
+			"src/win32_window.c",
+        }
+
+        defines {
+            "_GLFW_WIN32",
+			"_CRT_SECURE_NO_WARNINGS"
+        }
+
+    filter "configurations:Debug"
+        symbols "On"
+
+    filter "configurations:Release"
+        optimize "On"
